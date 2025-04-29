f082b47fd32a3536722186e14b5a14dba1dade79
TokolohoMohale <150892343+TokolohoMohale@users.noreply.github.com>
2025-04-29 20:21:37 +0200

    chore: added and setted up mantine

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,20 +1,25 @@
-import type { ReactNode } from 'react'
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import '@mantine/core/styles.css'
+
+import { ColorSchemeScript, MantineProvider } from '@mantine/core'
 
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
 import FatalErrorPage from 'src/pages/FatalErrorPage'
-
+import Routes from 'src/Routes'
 import './index.css'
 
-interface AppProps {
-  children?: ReactNode
-}
-
-const App = ({ children }: AppProps) => (
+const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
     <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
-      <RedwoodApolloProvider>{children}</RedwoodApolloProvider>
+      <ColorSchemeScript />
+      <MantineProvider>
+        <RedwoodApolloProvider>
+          <Routes />
+        </RedwoodApolloProvider>
+      </MantineProvider>
     </RedwoodProvider>
   </FatalErrorBoundary>
 )
